d608318dfb20c0fd42d4e14c8945337666f41972
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-15 14:59:40 -0400

    Add migration script for website_event_filter_selector

---
+++ website_event_filter_selector/migrations/12.0.1.2.0/pre-migration.py
@@ -0,0 +1,10 @@
+
+from openupgradelib import openupgrade
+from .common import views_to_update
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    for view_ref in views_to_update:
+        view = env.ref(view_ref)
+        view.active = False
